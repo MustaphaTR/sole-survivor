94e03fcfd29d79e77bc6b81e65c54ddbd37e68f7
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-10-27 15:23:02 +0300

    Update engine to lastest bleed and add crate notifications

--- OpenRA.Mods.SS/Traits/Crates/SSHealCrateAction.cs
+++ OpenRA.Mods.SS/Traits/Crates/SSHealCrateAction.cs
@@ -21,17 +21,22 @@ namespace OpenRA.Mods.Common.Traits
         [Desc("The weapon to fire if collector has full health.")]
         [WeaponReference, FieldLoader.Require] public string Weapon = null;
 
+        [Desc("Notification to play when the crate is collected and weapon fires.")]
+        [NotificationReference("Speech")] public readonly string WeaponNotification = null;
+
         public override object Create(ActorInitializer init) { return new SSHealCrateAction(init.Self, this); }
 	}
 
 	class SSHealCrateAction : CrateAction
     {
+        readonly Actor self;
         readonly SSHealCrateActionInfo info;
 
         public SSHealCrateAction(Actor self, SSHealCrateActionInfo info)
 			: base(self, info)
         {
             this.info = info;
+            this.self = self;
         }
 
         public override void Activate(Actor collector)
@@ -41,12 +46,20 @@ namespace OpenRA.Mods.Common.Traits
             {
                 var weapon = collector.World.Map.Rules.Weapons[info.Weapon.ToLowerInvariant()];
                 weapon.Impact(Target.FromPos(collector.CenterPosition), collector, Enumerable.Empty<int>());
+
+                if (!string.IsNullOrEmpty(info.WeaponNotification))
+                    Game.Sound.PlayNotification(self.World.Map.Rules, collector.Owner, "Speech",
+                        info.WeaponNotification, collector.Owner.Faction.InternalName);
             }
             else
             {
                 health.InflictDamage(collector, collector, new Damage(-(health.MaxHP - health.HP)), true);
+                
+                if (!string.IsNullOrEmpty(Info.Notification))
+                    Game.Sound.PlayNotification(self.World.Map.Rules, collector.Owner, "Speech",
+                        Info.Notification, collector.Owner.Faction.InternalName);
 
-                Game.Sound.PlayToPlayer(SoundType.World, collector.Owner, info.Notification);
+                Game.Sound.Play(SoundType.World, info.Sound, self.CenterPosition);
                 if (info.Effect != null)
                     collector.World.AddFrameEndTask(w => w.Add(new CrateEffect(collector, info.Effect, info.Palette)));
             }
