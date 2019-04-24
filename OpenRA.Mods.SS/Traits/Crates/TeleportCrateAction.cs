d593f57c43c150e0b6cfc72361210395947eae7d
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2019-04-24 09:59:43 +0300

    Update mod to lastest bleed.

--- OpenRA.Mods.SS/Traits/Crates/TeleportCrateAction.cs
+++ OpenRA.Mods.SS/Traits/Crates/TeleportCrateAction.cs
@@ -10,10 +10,10 @@
 #endregion
 
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using OpenRA.Graphics;
 using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
 using OpenRA.Traits;
 
 namespace OpenRA.Mods.SS.Traits
