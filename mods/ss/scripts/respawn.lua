9c97b4dc902f41a3c92e540481ce4b7b8d07e27a
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2019-06-23 21:38:42 +0300

    Change respawn code to use triggers instead of checking stuff every tick.

--- mods/ss/scripts/respawn.lua
+++ mods/ss/scripts/respawn.lua
@@ -18,51 +18,22 @@ RespawnDelay =
 	ten = DateTime.Seconds(10)
 }
 
-RespawnTimers = { }
-SetupRespawnTimers = function()
-	for _,player in pairs(players) do
-		RespawnTimers[player.InternalName] = RespawnDelay[RespawnOption]
-	end
-end
-
-RespawnTick = function()
-	for _,player in pairs(players) do
+Respawn = function(player)
+	Trigger.OnKilled(Units[player.InternalName], function()
 		if not player.IsObjectiveFailed(0) then
-			if Units[player.InternalName] ~= nil then
-				if Units[player.InternalName].IsDead then
-					if RespawnOption ~= "disabled" then
-						if RespawnTimers[player.InternalName] == RespawnDelay[RespawnOption] then
-							player.Experience = player.Experience - 20
-						end
+			player.Experience = player.Experience - 20
 
-						RespawnTimers[player.InternalName] = RespawnTimers[player.InternalName] - 1
+			Trigger.AfterDelay(RespawnDelay[RespawnOption], function()
+				local unitType = Units[player.InternalName].Type
 
-						if RespawnTimers[player.InternalName] < 0 then
-							local unitType = Units[player.InternalName].Type
-
-							Units[player.InternalName] = Actor.Create(unitType, true, { Owner = player, Location = player.SpawnCellPosition })
-							RespawnTimers[player.InternalName] = RespawnDelay[RespawnOption]
-							if player.IsLocalPlayer then
-								Camera.Position = player.SpawnWorldPosition
-							end
-						end
-					else
-						player.MarkFailedObjective(0)
-					end
+				Units[player.InternalName] = Actor.Create(unitType, true, { Owner = player, Location = player.SpawnCellPosition })
+				if player.IsLocalPlayer then
+					Camera.Position = player.SpawnWorldPosition
 				end
-			end
-
-			local win = true
-			for _,enemy in pairs(Utils.Where(players, function(p) return p ~= player and (p.Team == 0 or p.Team ~= player.Team) end)) do
-				if not enemy.IsObjectiveFailed(0) then
-					win = false
-				end
-			end
-			if win then
-				player.MarkCompletedObjective(0)
-			end
+				Respawn(player)
+			end)
 		end
-	end
+	end)
 end
 
 SetupObjectives = function()
@@ -128,10 +99,34 @@ SetupObjectives = function()
 	end
 end
 
+RespawnTick = function()
+	for _,player in pairs(players) do
+		if not player.IsObjectiveFailed(0) then
+			local win = true
+			for _,enemy in pairs(Utils.Where(players, function(p) return p ~= player and (p.Team == 0 or p.Team ~= player.Team) end)) do
+				if not enemy.IsObjectiveFailed(0) then
+					win = false
+				end
+			end
+			if win then
+				player.MarkCompletedObjective(0)
+			end
+		end
+	end
+end
+
 RespawnWorldLoaded = function()
 	players = Player.GetPlayers(function(p) return not p.IsNonCombatant end)
 	neutral = Player.GetPlayer("Neutral")
 
-	SetupRespawnTimers()
 	SetupObjectives()
+	for _,player in pairs(players) do
+		if RespawnOption ~= "disabled" then
+			Respawn(player)
+		else
+			Trigger.OnKilled(Units[player.InternalName], function()
+				player.MarkFailedObjective(0)
+			end)
+		end
+	end
 end
