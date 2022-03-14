05fd457b290500c78cb2ec45e59e92bf8521d581
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2022-03-14 14:20:06 +0300

    Engine update.

--- OpenRA.Mods.SS/Scripting/Properties/PositionableProperties.cs
+++ OpenRA.Mods.SS/Scripting/Properties/PositionableProperties.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -9,6 +9,7 @@
  */
 #endregion
 
+using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.SS.Traits;
 using OpenRA.Scripting;
 using OpenRA.Traits;
