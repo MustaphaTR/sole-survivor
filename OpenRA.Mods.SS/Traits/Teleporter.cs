3285d01245883f9fa50d5e7a94d82e7b8852a92c
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2022-06-05 17:48:48 +0300

    Teleporters are not crates.

--- OpenRA.Mods.SS/Traits/Teleporter.cs
+++ OpenRA.Mods.SS/Traits/Teleporter.cs
@@ -1,6 +1,6 @@
 #region Copyright & License Information
 /*
- * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of
@@ -214,19 +214,11 @@ namespace OpenRA.Mods.SS.Traits
 		void INotifyAddedToWorld.AddedToWorld(Actor self)
 		{
 			self.World.AddToMaps(self, this);
-
-			var cs = self.World.WorldActor.TraitOrDefault<SSCrateSpawner>();
-			if (cs != null)
-				cs.IncrementCrates();
 		}
 
 		void INotifyRemovedFromWorld.RemovedFromWorld(Actor self)
 		{
 			self.World.RemoveFromMaps(self, this);
-
-			var cs = self.World.WorldActor.TraitOrDefault<SSCrateSpawner>();
-			if (cs != null)
-				cs.DecrementCrates();
 		}
 
 		public IEnumerable<IRenderable> Render(Actor self, WorldRenderer wr)
